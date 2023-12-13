84f2b105db6ac35be894614b8c4418520514acee
Fguedes10 <fadg100@gmail.com>
2023-12-13 15:53:10 +0000

    Add class message, server and client

---
+++ src/Messages/Message.java
@@ -0,0 +1,11 @@
+package Messages;
+
+public abstract class Message {
+
+    private final static String WELCOME = "Welcome to Cards Against Mindera";
+    private final static String WINNER = " wins the game!";
+    private final static String ROUND = "Round Nº";
+    private final static String SCORE = "";
+
+
+}
