c2200d60e9803784b38cebf93509386b85827f9f
josermsribeiro <joseruysaribeiro@icloud.com>
2023-12-14 14:42:35 +0000

    Latest Commit

---
+++ src/Commands/NewGameHandler.java
@@ -0,0 +1,11 @@
+package Commands;
+
+import Server.Server;
+
+public class NewGameHandler implements CommandHandler{
+
+    @Override
+    public void execute(Server server, Server.ClientConnectionHandler clientConnectionHandler) {
+
+    }
+}
---
+++ src/Commands/QuitHandler.java
@@ -0,0 +1,11 @@
+package Commands;
+
+import Server.Server;
+
+public class QuitHandler implements CommandHandler{
+
+    @Override
+    public void execute(Server server, Server.ClientConnectionHandler clientConnectionHandler) {
+        
+    }
+}
