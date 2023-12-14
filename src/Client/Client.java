2110aa0b5ca20bdd91186fbe441f880de137ead5
JoaoRibeiroMoutinho <joaocmribeiro1@gmail.com>
2023-12-14 11:18:44 +0000

    first commit client

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,4 +1,40 @@
 package Client;
 
+import java.util.List;
+
 public class Client {
+
+    private String name;
+    private int age;
+    private int score;
+    private boolean voteState;
+    private List<Cards> hand;
+
+
+
+    public Client(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean requestHand() {
+        return true;
+    }
+
+    public fillHand() {
+
+    }
+    public Cards pickCard(Cards card) {
+        return card;
+    }
+    public int voteWinningHand() {
+        return 0;
+    }public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
 }
