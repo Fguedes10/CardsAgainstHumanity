95c14cf225002a27f5bc5cc543241789c8b4386b
JoaoRibeiroMoutinho <joaocmribeiro1@gmail.com>
2023-12-14 14:07:35 +0000

    second commit

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,5 +1,6 @@
 package Client;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,36 +15,33 @@ public class Client {
         this.name = name;
         this.age = age;
         this.score = 0;
-        this.cards = new List<>();
+        this.cards = new ArrayList<>();
         this.voteState = false;
     }
 
     public void requestHand() {   //
     }
-
-    public synchronized void fillHand() {   //verify if number of cards in hand and fill hand
+    public synchronized void fillHand(List<String> newCards) {
+        // Verify if the number of cards in hand and fill hand
+        if (cards.size() < 4) {
+            cards.addAll(newCards);
+        }
     }
 
-    public void pickCard() {  //pick card from hand to play
+
+    public synchronized void pickCard(String card) {
+        // Pick a card from hand to play
+        if (cards.contains(card)) {
+            cards.remove(card);
+            // Implement logic for playing the card
+        }
     }
 
-    public void voteWinningHand() {  //if voteState true chose number of player to vote
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    public synchronized void voteWinningHand() {
+        // If voteState is true, choose the number of the player to vote
+        if (voteState) {
+            // Implement logic for voting
+        }
     }
 
     public int getScore() {
@@ -54,18 +52,6 @@ public class Client {
         this.score = score;
     }
 
-    public List<String> getCards() {
-        return cards;
-    }
-
-    public void setCards(List<String> cards) {
-        this.cards = cards;
-    }
-
-    public Object getVoteState() {
-        return voteState;
-    }
-
     public boolean isVoteState() {
         return voteState;
     }
@@ -74,3 +60,13 @@ public class Client {
         this.voteState = voteState;
     }
 }
+
+
+
+
+
+
+
+
+
+
