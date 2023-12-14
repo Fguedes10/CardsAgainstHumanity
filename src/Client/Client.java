212b21df07fb43aea1b4cc873848ea17765d34f8
JoaoRibeiroMoutinho <joaocmribeiro1@gmail.com>
2023-12-14 11:36:26 +0000

    first commit client

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,40 +1,72 @@
 package Client;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
-
     private String name;
     private int age;
     private int score;
-    private boolean voteState;
-    private List<String> cards;
-
-
+    private List<Card> cards;
+    private Object voteState;
 
     public Client(String name, int age) {
         this.name = name;
         this.age = age;
+        this.score = 0;
+        this.cards = new ArrayList<>();
+        this.voteState = null;
     }
 
-    public boolean requestHand() {
-        return true;
+    public void requestHand() {
     }
 
-    public fillHand() {
+    public synchronized void fillHand() {
+    }
 
+    public void pickCard() {
     }
-    public Cards pickCard(Cards card) {
-        return card;
+
+    public void voteWinningHand() {
     }
-    public int voteWinningHand() {
-        return 0;
-    }public String getName() {
+
+    public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getAge() {
         return age;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public Object getVoteState() {
+        return voteState;
+    }
+
+    public void setVoteState(Object voteState) {
+        this.voteState = voteState;
+    }
 }
