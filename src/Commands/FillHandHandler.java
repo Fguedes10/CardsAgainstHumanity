576b1b189e601dd84344a9d01c31c82b972b6f81
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2023-12-18 11:38:23 +0000

    here's to hope

--- src/Commands/FillHandHandler.java
+++ src/Commands/FillHandHandler.java
@@ -1,9 +1,11 @@
 package Commands;
 
 import Client.ClientConnectionHandler;
+import Game.Card;
 import Server.Server;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FillHandHandler implements CommandHandler {
@@ -13,16 +15,39 @@ public class FillHandHandler implements CommandHandler {
         ClientConnectionHandler owner = clientConnectionHandler.getPlayingGame().owner;
         clientConnectionHandler.getCorrespondingClient().fillHand();
         clientConnectionHandler.writeMessage("You have now picked cards.");
-        List<String> cards = clientConnectionHandler.getCorrespondingClient().getCards();
+       /* List<String> cards = clientConnectionHandler.getCorrespondingClient().getCards();
         int index = 1;
         for (String card : cards) {
             try {
-                clientConnectionHandler.writeMessage(index + " - " + card);
+                clientConnectionHandler.writeMessage(Card.drawWhiteCard(card, index));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
             index++;
+        }*/
+
+        List<String> cards = clientConnectionHandler.getCorrespondingClient().getCards();
+        List<List<String>> cardLinesList = new ArrayList<>();
+        int maxLines = 0;
+        int index = 1;
+        // Get lines for each card and find the max number of lines
+        for (String card : cards) {
+            List<String> cardLines = Card.drawHand(card, index++);
+            cardLinesList.add(cardLines);
+            maxLines = Math.max(maxLines, cardLines.size());
         }
-            owner.send(clientConnectionHandler.getName() + " has picked their hand!");
+
+        // Construct each line of the hand
+        for (int i = 0; i < maxLines; i++) {
+            StringBuilder handLine = new StringBuilder();
+            for (List<String> cardLines : cardLinesList) {
+                handLine.append(cardLines.size() > i ? cardLines.get(i) : " ".repeat(22)); // 22 is the card width
+            }
+            clientConnectionHandler.writeMessage(handLine.toString());
+        }
+
+
+
+        owner.send(clientConnectionHandler.getName() + " has picked their hand!");
     }
 }
